afdfcc2d393ce91eb2d6d21c7afd72f0c78ca006
carykees98 <trainboy980@gmail.com>
2023-11-05 06:25:21 +0000

    load config and configure logger

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
+// STD Includes
+#include <fstream>
 #include <iostream>
 
+// Library Includes
+#include <nlohmann/json.hpp>
+#include <mirror/logger.h>
+
+using json = nlohmann::json;
+
 int main() {
-    std::cout << "Hello World!" << std::endl;
-    return 0;
+    // Load Config
+    json config{};
+    {
+        std::ifstream configFile("../config.json");
+        config = json::parse(configFile);
+    }
+
+    // Configure Logger
+    mirror::Logger *logger = mirror::Logger::getInstance();
+    logger->configure(config["port"].get<uint16_t>(), "mirror-api");
+
+    std::exit(EXIT_SUCCESS);
 }
